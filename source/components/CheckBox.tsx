e86ec721a0309a9294e55ab436fcbfe650e46853
Timur <timurtokaev.toombez@gmail.com>
2023-03-06 01:39:38 +0300

    Add Label

--- source/components/CheckBox.tsx
+++ source/components/CheckBox.tsx
@@ -1,23 +1,40 @@
-import React, { useState } from 'react'
-import { Box, Text, useInput } from "ink"
+import React, { useState, useEffect } from 'react'
+import { Box, Text, useInput, TextProps, BoxProps } from "ink"
 
-import { useFocusHandler, UseFocusHandlerProps } from '../hooks/useFocusHandler.js'
+import useFocusHandler, { UseFocusHandlerProps } from '../hooks/useFocusHandler.js'
 
 interface CheckBoxIndicatorProps {
+    isFocused?: boolean
     isChecked?: boolean
+    textProps?: TextProps
 }
 
 const CheckBoxIndicator: React.FC<CheckBoxIndicatorProps> = ({
     isChecked,
+    textProps,
 }) => {
     const checkBoxSymbol = isChecked ? '□' : '■'
 
-    return <Text>{ checkBoxSymbol }</Text>
+    return <Text {...textProps} >{ checkBoxSymbol }</Text>
+}
+
+interface CheckBoxLabelProps {
+    label?: string
+    isFocused?: boolean
+    textProps?: TextProps
+}
+
+const CheckBoxLabel: React.FC<CheckBoxLabelProps> = ({
+    label,
+    textProps,
+}) => {
+    return <Text {...textProps} >{label}</Text>
 }
 
 interface CheckBoxProps {
     onFocus?: UseFocusHandlerProps['handler']
     onChange?: (value: boolean) => void
+    label?: string
     focusOptions?: UseFocusHandlerProps['focusOptions']
     boxProps?: BoxProps
 }
@@ -25,6 +42,7 @@ interface CheckBoxProps {
 const CheckBox: React.FC<CheckBoxProps> = ({
     onFocus,
     onChange,
+    label,
     focusOptions,
     boxProps,
 }) => {
@@ -57,6 +75,11 @@ const CheckBox: React.FC<CheckBoxProps> = ({
 
     return <Box {...boxProps} >
         <CheckBoxIndicator isChecked={isChecked} isFocused={isFocused} />
+        { label &&
+            <Box paddingLeft={1}>
+                <CheckBoxLabel label={label} isFocused={isFocused} />
+            </Box>
+        }
     </Box>
 }
 
