b2b1d054d228faaf3f112c6d3b50adc238228073
Timur <timurtokaev.toombez@gmail.com>
2023-03-06 19:32:46 +0300

    Button example

--- source/ui.tsx
+++ source/ui.tsx
@@ -1,10 +1,36 @@
-import React, {type FC} from 'react'
-import {Text} from 'ink'
+import React, {useState, type FC} from 'react'
+import {Box, Text} from 'ink'
+import Button, {  } from './components/Button.js'
 
-const App: FC<{name?: string}> = ({name = 'Stranger'}) => (
-    <Text>
-        Hello, <Text color='green'>{name}</Text>
-    </Text>
-)
+const App: FC<{name?: string}> = ({name = 'Stranger'}) => {
+    const [c, setC] = useState(0)
 
+    return <Box>
+        <Button
+            wrapperComponent={({ isFocused, children }) => {
+                return <Box><Text>{`${isFocused}`}|</Text>{children}</Box>
+            }}
+            contentComponent={() => {
+                return <Box borderStyle='single'>
+                    <Text>c++</Text>
+                </Box>
+            }}
+            onClick={() => setC(v => ++v)}
+        />
+        <Button
+            wrapperComponent={({ isFocused, children }) => {
+                return <Box><Text>{`${isFocused}`}|</Text>{children}</Box>
+            }}
+            contentComponent={() => {
+                return <Box borderStyle='single'>
+                    <Text>c--</Text>
+                </Box>
+            }}
+            onClick={() => setC(v => --v)}
+        />
+        <Text>
+            { c }
+        </Text>
+    </Box>
+}
 export default App
