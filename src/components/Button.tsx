99cbd4d96d2771330910b12a97f04e267596aa86
Timur <timurtokaev.toombez@gmail.com>
2023-03-28 17:07:51 +0300

    Move source to src

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,28 +3,45 @@ import { Box, useInput } from "ink"
 import useFocusHandler, { UseFocusHandlerProps } from '../hooks/useFocusHandler.js'
 import { InkChildren } from '../types.js'
 
+/**
+ * Props for button wrapper component
+ */
 interface ButtonWrapperProps {
     isFocused?: boolean
     children?: InkChildren
 }
 
+/**
+ * Component for wrap `content` passed in button
+ * @param props
+ */
 const ButtonWrapper: React.FC<ButtonWrapperProps> = ({
     children
 }) => {
     return <Box>{children}</Box>
 }
 
+/**
+ * Props for button content component
+ */
 interface ButtonContentProps {
     isFocused?: boolean
     children?: InkChildren
 }
 
+/**
+ * Component for displaying button `content` passed as children
+ * @param props
+ */
 const ButtonContent: React.FC<ButtonContentProps> = ({children}) => {
     return <>
         {children}
     </>
 }
 
+/**
+ * Props for root `button` component
+ */
 interface ButtonProps {
     onClick?: () => void
     onFocus?: UseFocusHandlerProps['handler']
@@ -34,6 +51,10 @@ interface ButtonProps {
     contentComponent?: React.FC<ButtonContentProps>
 }
 
+/**
+ * Component for root button element
+ * @param props
+ */
 const Button: React.FC<ButtonProps> = ({
     wrapperComponent = ButtonWrapper,
     contentComponent = ButtonContent,
@@ -60,4 +81,13 @@ const Button: React.FC<ButtonProps> = ({
 }
 
 export default Button
-export { Button }
+export {
+    ButtonWrapper,
+    ButtonContent,
+    Button,
+}
+export type {
+    ButtonWrapperProps,
+    ButtonContentProps,
+    ButtonProps,
+}
