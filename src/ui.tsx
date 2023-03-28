b3868b6eb92717a2f11886b8bd3fcc19bf64f30d
Timur <timurtokaev.toombez@gmail.com>
2023-03-28 18:37:29 +0300

    Add Input

--- src/ui.tsx
+++ src/ui.tsx
@@ -1,8 +1,15 @@
+import { Box } from 'ink'
 import React from 'react'
 import ButtonExample from './examples/Button.js'
+import InputExample from './examples/Input.js'
 
-const App = ({ name }: { name?: string }) => (
-    <ButtonExample />
-)
+const App = ({ name }: { name?: string }) => {
+    return (
+        <Box>
+            {/* <ButtonExample /> */}
+            <InputExample />
+        </Box>
+    )
+}
 
 export default App
