919444548b93ede2d9368df77edc85baef45306d
Timur <timurtokaev.toombez@gmail.com>
2023-05-12 17:28:17 +0300

    Change button rendering

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,40 @@
-import { Text } from 'ink'
 import React from 'react'
-import { handler, useFocusOptions } from '../types.js'
+import { InkChildren, handler, useFocusOptions } from '../types.js'
 import { useButton } from '../hooks/useButton.js'
 
+/**
+ * Button render props
+ */
+interface ButtonRenderProps {
+    isFocused?: boolean
+    children?: InkChildren
+}
+
+/**
+ * Button render props omited children
+ */
+type ButtonRenderPropsWithoutChildrent = Omit<ButtonRenderProps, 'children'>
+
+/**
+ * Button render function
+ */
+
+type ButtonRender = React.FC<ButtonRenderProps>
+/**
+ * Button render function without children
+ */
+type ButtonRenderWithoutChildren = React.FC<ButtonRenderPropsWithoutChildrent>
+
+/**
+ * Button props
+ */
 interface ButtonProps {
     onClick?: handler
     onFocus?: handler
     onBlur?: handler
 
+    children?: ButtonRenderWithoutChildren | InkChildren
+    render?: ButtonRender
     focusOptions?: useFocusOptions
 }
 
@@ -16,6 +43,8 @@ const Button: React.FC<ButtonProps> = ({
     onClick,
     onFocus,
 
+    children,
+    render = ({ children }) => <>{children}</>,
     focusOptions,
 }) => {
     const { isFocused } = useButton({
@@ -26,11 +55,16 @@ const Button: React.FC<ButtonProps> = ({
         focusOptions,
     })
 
-    return (
-        <Text underline={isFocused}>
-            Lorem, ipsum.
-        </Text>
-    )
+    const _children = typeof children === 'function'
+        ? React.createElement(children, { isFocused })
+        : children
+
+    const _render = React.createElement(render, {
+        isFocused,
+        children: _children
+    })
+
+    return _render
 }
 
 export default Button
