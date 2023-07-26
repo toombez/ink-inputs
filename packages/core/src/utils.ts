97fbf4ab6e010320cca29dd28def8483f680b68d
Timur <timurtokaev.toombez@gmail.com>
2023-07-26 23:40:24 +0300

    Add moduloSequence

--- packages/core/src/utils.ts
+++ packages/core/src/utils.ts
@@ -1,3 +1,10 @@
 export function clamp(number: number, max: number, min: number) {
     return Math.min(Math.max(number, min), max)
 }
+
+export function moduloSequence(number: number, max: number, min: number = 0) {
+    const length = max - min
+
+    return (number % length + length - min) % length + min
+}
+
