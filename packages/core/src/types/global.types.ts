a7c94ad3d6bb962df59c2fa71775e0c98a9e4da9
Timur <toombez.timurtokaev@gmail.com>
2023-08-20 22:28:33 +0300

    Add global types

---
+++ packages/core/src/types/global.types.ts
@@ -0,0 +1,84 @@
+import { useFocus } from 'ink'
+
+// =========================
+// Ink.js helper types
+// =========================
+
+/**
+ * Type for ink components childrens
+ */
+export type InkChildren = React.ReactNode
+
+/**
+ * Use focus options
+ */
+export type UseFocusOptions = Parameters<typeof useFocus>[0]
+
+// =========================
+// Ink-inputs general types
+// =========================
+
+/**
+ * Handler type for inputs
+ */
+export type Handler = () => void
+
+/**
+ * Input render component type
+ */
+export type InputRender<
+    T extends BaseRenderProps = BaseRenderProps,
+> = React.FC<T>
+
+/**
+ * Input children
+ */
+export type InputChildren<
+    T extends BaseRenderProps = BaseRenderProps,
+> = InkChildren | InputRender<T>
+
+// =========================
+// Ink-inputs base props types
+// =========================
+
+/**
+ * Input render common props
+ */
+export type BaseRenderProps = {
+    /**
+     * Is focused input
+     */
+    isFocused: boolean
+}
+
+/**
+ * Input common props
+ */
+export type BaseInputProps<
+    T extends BaseRenderProps = BaseRenderProps,
+> = {
+    /**
+     * Render component or static element
+     */
+    children?: InputChildren<T>
+
+    /**
+     * Render component
+     */
+    render?: InputRender<T>
+
+    /**
+     * `useFocus` hook options
+     */
+    focusOptions?: UseFocusOptions
+
+    /**
+     * Focus handler
+     */
+    onFocus?: Handler
+
+    /**
+     * Blur handler
+     */
+    onBlur?: Handler
+}
