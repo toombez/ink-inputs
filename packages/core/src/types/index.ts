2f28630624d41ff0848cd9cd49522867260c4fbc
Timur <toombez.timurtokaev@gmail.com>
2023-08-22 03:25:17 +0300

    Move global to types/index.ts

--- packages/core/src/types/index.ts
+++ packages/core/src/types/index.ts
@@ -1,7 +1,39 @@
-export type * from './global.types.js'
-export type * from './useButton.types.js'
-export type * from './useCheckBox.types.js'
-export type * from './useRadio.types.js'
-export type * from './useTextInput.types.js'
-export type * from './useCursor.js'
-export type * from './useSelect.types.js'
+import { useFocus } from "ink"
+
+export type InkChildren = React.ReactNode
+export type UseFocusOptions = NonNullable<Parameters<typeof useFocus>[0]>
+export type UseFocusResult = ReturnType<typeof useFocus>
+
+export type InputRender<
+    T extends BaseRenderProps = BaseRenderProps
+> = React.FC<T>
+export type InputChildren<
+    T extends BaseRenderProps = BaseRenderProps
+> = InkChildren | InputRender<T>
+
+export type BaseInputProps<
+    T extends BaseRenderProps = BaseRenderProps
+> = {
+    id?: UseFocusOptions['id']
+    autoFocus?: UseFocusOptions['autoFocus']
+    isDisabled?: boolean
+    children?: InputChildren<T>
+    render?: InputRender<T>
+
+    onFocus?: () => void
+    onBlur?: () => void
+}
+
+export type BaseRenderProps = {
+    isDisabled: boolean
+    isFocused: UseFocusResult['isFocused']
+    focus: UseFocusResult['focus']
+}
+
+export type PropsBuilder<
+    T extends Object = {},
+    K extends Object = {},
+> = {
+    InputProps: T & BaseInputProps<K & BaseRenderProps>
+    RenderProps: K & BaseRenderProps
+}
