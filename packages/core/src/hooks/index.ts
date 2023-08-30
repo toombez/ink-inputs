bcc231b7c3e1caf413c90a96f4cb9aba9634893b
Timur <toombez.timurtokaev@gmail.com>
2023-08-30 20:51:01 +0300

    Export new hooks and types

--- packages/core/src/hooks/index.ts
+++ packages/core/src/hooks/index.ts
@@ -1,9 +1,16 @@
-export { UseBaseInput } from './useBaseInput/index.js'
+export { useBaseInput } from './useBaseInput/index.js'
+export {
+    useCursor,
+    CURSOR_DEFAULT_MINIMAL_POSITION
+} from './useCursor/index.js'
+export {
+    useWideCursor,
+    CURSOR_MINIMAL_WIDTH
+} from './useWideCursor/index.js'
+export { useRender } from './useRender/index.js'
 export { useButton } from './useButton/index.js'
-export { useCursor } from './useCursor/index.js'
 export { useCheckBox } from './useCheckBox/index.js'
 export { useRadio } from './useRadio/index.js'
-export { useRender } from './useRender/index.js'
 export { useSelect } from './useSelect/index.js'
 export { useTextInput } from './useTextInput/index.js'
 
@@ -12,13 +19,20 @@ export type {
     UseBaseInputResult,
 } from './useBaseInput/index.js'
 export type {
+    UseCursorOptions,
+    UseCursorOutput,
+} from './useCursor/index.js'
+export type {
+    IRenderChildrenProps,
+} from './useRender/index.js'
+export type {
+    UseWideCursorOptions,
+    UseWideCursorOutput,
+} from './useWideCursor/index.js'
+export type {
     ButtonProps,
     ButtonRenderProps,
 } from './useButton/index.js'
-export type {
-    UseCursorOptions,
-    UseCursorResult,
-} from './useCursor/index.js'
 export type {
     CheckBoxOption,
     CheckBoxProps,
@@ -30,16 +44,12 @@ export type {
     RadioRenderProps,
 } from './useRadio/index.js'
 export type {
-    IRenderChildrenProps,
-} from './useRender/index.js'
-export type {
     SelectOption,
     SelectProps,
     SelectRenderProps,
 } from './useSelect/index.js'
-
 export type {
-    TextInputCursorOperations,
+    TEXT_INPUT_KEY_OPERATION,
     TextInputProps,
     TextInputRenderProps,
 } from './useTextInput/index.js'
