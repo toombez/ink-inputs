ecf2b27ea4d4a2d55d09751527fbd79759d6b2d0
Timur <toombez.timurtokaev@gmail.com>
2023-08-30 12:39:31 +0300

    Add useWideCursor hook

---
+++ packages/core/src/hooks/useWideCursor/index.ts
@@ -0,0 +1,9 @@
+export {
+    CURSOR_MINIMAL_WIDTH,
+    useWideCursor,
+} from './useWideCursor.hook.js'
+
+export type {
+    UseWideCursorOptions,
+    UseWideCursorOutput,
+} from './useWideCursor.types.js'
---
+++ packages/core/src/hooks/useWideCursor/useWideCursor.hook.ts
@@ -0,0 +1,93 @@
+import {
+    CURSOR_DEFAULT_MINIMAL_POSITION,
+    useCursor
+} from "@hooks/useCursor/index.js"
+import { useState, useEffect } from "react"
+import { UseWideCursorOptions, UseWideCursorOutput } from "./useWideCursor.types.js"
+import { clamp } from "@/utils.js"
+
+export const CURSOR_MINIMAL_WIDTH = 1 as const
+
+export function useWideCursor({
+    initialWidth = CURSOR_MINIMAL_WIDTH,
+    maxPosition,
+    minPosition = CURSOR_DEFAULT_MINIMAL_POSITION,
+    ...restUseWideCursorOptions
+}: UseWideCursorOptions): UseWideCursorOutput {
+    const {
+        cursorPosition,
+
+        moveCursorToEnd,
+        moveCursorToStart,
+
+        ...restUseCursorOutput
+    } = useCursor({
+        ...restUseWideCursorOptions,
+        maxPosition,
+        minPosition,
+    })
+
+    useEffect(() => {
+        if (cursorPosition + initialWidth > maxPosition) {
+            throw "Cursor out bounds."
+        }
+    }, [])
+
+    const [cursorWidth, setCursorWidth] = useState<number>(initialWidth)
+
+    const allowedMaxWidth = Math.max(
+        maxPosition - cursorPosition,
+        CURSOR_MINIMAL_WIDTH
+    )
+    const indexAfterCursor = cursorPosition + cursorWidth
+    const isCursorMinimalWidth = cursorWidth === CURSOR_MINIMAL_WIDTH
+
+    function resizeCursorTo(width: number) {
+        setCursorWidth(clamp(width, CURSOR_MINIMAL_WIDTH, allowedMaxWidth))
+    }
+
+    function resizeCursor(delta: number) {
+        resizeCursorTo(cursorWidth + delta)
+    }
+
+    function resizeCursorToMinimal() {
+        resizeCursorTo(CURSOR_MINIMAL_WIDTH)
+    }
+
+    function resizeCursorToMaximal() {
+        resizeCursorTo(allowedMaxWidth)
+    }
+
+    useEffect(() => {
+        if (cursorWidth > allowedMaxWidth) {
+            resizeCursorToMaximal()
+        }
+    }, [cursorPosition])
+
+    useEffect(() => {
+        if (cursorPosition > maxPosition) {
+            return moveCursorToEnd()
+        }
+
+        if (cursorPosition < minPosition) {
+            return moveCursorToStart()
+        }
+    }, [maxPosition, minPosition])
+
+    return {
+        ...restUseCursorOutput,
+
+        cursorPosition,
+        cursorWidth,
+        indexAfterCursor,
+        isCursorMinimalWidth,
+
+        moveCursorToStart,
+        moveCursorToEnd,
+
+        resizeCursorTo,
+        resizeCursor,
+        resizeCursorToMinimal,
+        resizeCursorToMaximal,
+    }
+}
