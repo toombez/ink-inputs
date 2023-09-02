15163dda7cf15643cece5d49197bdb5ab9046acb
Timur <toombez.timurtokaev@gmail.com>
2023-09-03 00:02:58 +0300

    Move Radio to core package

---
+++ packages/core/src/inputs/Radio/Radio.types.ts
@@ -0,0 +1,30 @@
+import { UseCursorOutput } from "@hooks/index.js"
+import {
+    CustomRenderElementProps,
+    FocusableElementProps,
+    FocusableElementRenderProps,
+    InputElementProps,
+    InputElementRenderProps,
+    OpenableElementProps,
+    OpenableElementRenderProps,
+    Option,
+} from "@types"
+
+export type RadioValue<T> = Option<T> | null
+
+export type RadioRenderProps<T> = {
+    options: Array<Option<T>>
+    valueIndex: number | null
+    cursorPosition: UseCursorOutput['cursorPosition']
+}
+    & FocusableElementRenderProps
+    & InputElementRenderProps<RadioValue<T>>
+    & OpenableElementRenderProps
+
+export type RadioProps<T> = {
+    options: Array<Option<T>>
+}
+    & FocusableElementProps
+    & InputElementProps<RadioValue<T>>
+    & CustomRenderElementProps<RadioRenderProps<T>>
+    & OpenableElementProps
