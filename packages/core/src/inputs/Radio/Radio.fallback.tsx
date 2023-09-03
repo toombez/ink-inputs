3acc712aee805ff597928e98932b11a5608c89a3
Timur <toombez.timurtokaev@gmail.com>
2023-09-03 22:49:45 +0300

    Fix Radio fallback

--- packages/core/src/inputs/Radio/Radio.fallback.tsx
+++ packages/core/src/inputs/Radio/Radio.fallback.tsx
@@ -1,27 +1,40 @@
 import React from 'react'
 import { Box, Text } from "ink";
 import { RadioRenderProps } from "./Radio.types.js";
+import figureSet from 'figures';
 
 const RadioFallback = <T, >({
-    cursorPosition,
     isFocused,
+    isDisabled,
     value,
     valueIndex,
+    placeholder,
     options,
-}: RadioRenderProps<T>) => {
-    return (
-        <Box>
-            {options.map((option, index) => (
-                <Text key={index} underline={valueIndex === index}>
-                    <Text inverse={option === value}>
-                        o
-                    </Text>
-                    {' '}
-                    {option.label}
-                </Text>
-            ))}
-        </Box>
-    )
-}
+    cursorPosition
+}: RadioRenderProps<T>) => (
+    <Box flexDirection='column'>
+        <Text
+            inverse={isFocused}
+            strikethrough={isDisabled}
+        >
+            {placeholder}
+        </Text>
+        {options.map((option, index) => (
+            <Text
+                key={index}
+                underline={isFocused && index === cursorPosition}
+                inverse={isFocused && index === cursorPosition}
+                strikethrough={isDisabled}
+            >
+                {valueIndex === index
+                    ? figureSet.radioOn
+                    : figureSet.radioOff
+                }
+                {' '}
+                {option.label}
+            </Text>
+        ))}
+    </Box>
+)
 
 export default RadioFallback
