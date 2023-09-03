6d499934e7db760c548bb094243d445370d92389
Timur <toombez.timurtokaev@gmail.com>
2023-09-03 22:35:50 +0300

    Change CheckBox

--- packages/core/src/inputs/CheckBox/CheckBox.fallback.tsx
+++ packages/core/src/inputs/CheckBox/CheckBox.fallback.tsx
@@ -1,32 +1,41 @@
 import React from "react"
 import { Box, Text } from "ink"
 import { CheckBoxRenderProps } from "./CheckBox.types.js"
+import figureSet from 'figures'
 
 const CheckBoxFallback = <T, >({
     isDisabled,
     isFocused,
     options,
-    value,
     valueIndexes,
     cursorPosition,
     placeholder,
-}: CheckBoxRenderProps<T>) => {
-    return (
-        <Box>
-            {
-                options.map((option, index) => (
-                    <Text
-                        key={index}
-                        underline={index === cursorPosition}
-                    >
-                        {valueIndexes.includes(index) ? 'o' : 'x'}
-                        {' '}
-                        {option.label}
-                    </Text>
-                ))
-            }
-        </Box>
-    )
-}
+}: CheckBoxRenderProps<T>) => (
+    <Box flexDirection="column">
+        <Text
+            strikethrough={isDisabled}
+            inverse={isFocused}
+        >
+            {placeholder}
+        </Text>
+        {
+            options.map((option, index) => (
+                <Text
+                    key={index}
+                    underline={isFocused && index === cursorPosition}
+                    inverse={isFocused && index === cursorPosition}
+                    strikethrough={isDisabled}
+                >
+                    {valueIndexes.includes(index)
+                        ? figureSet.checkboxOn
+                        : figureSet.checkboxOff
+                    }
+                    {" "}
+                    {option.label}
+                </Text>
+            ))
+        }
+    </Box>
+)
 
 export default CheckBoxFallback
